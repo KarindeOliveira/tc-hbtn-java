21af2f8156a54db0365228e9b76487e242796e1e
Karin Pinheiro <karindanoli@gmail.com>
2022-06-14 14:08:53 -0300

    Teste

---
+++ 0x00-java-hello_world/TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,44 @@
+public class InteiroPositivo {
+
+
+    int valor;
+
+    public InteiroPositivo(int valor) {
+        this.valor = valor;
+    }
+
+    public InteiroPositivo(String valor){
+    try {
+        int num = Integer.parseInt(valor);
+        setValor(num);
+    }catch (NumberFormatException e){
+        throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+    }
+    }
+
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        } else {
+            this.valor = valor;
+        }
+    }
+
+    public boolean ehPrimo(){
+    for (int j = 2; j<this.valor; j++){
+        if (this.valor % j==0){
+            return false;
+        }
+    }
+    if(this.valor == 1) {
+        return false;
+    }else{
+        return true;
+    }
+    }
+}
