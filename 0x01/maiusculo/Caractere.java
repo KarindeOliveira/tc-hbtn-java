15865877ff7a6dd73c3626ef33b6487191c0b070
Karin Pinheiro <karindanoli@gmail.com>
2022-06-20 17:53:19 -0300

    Teste

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,16 @@
+public class Caractere {
+
+
+    public static boolean EhMaiusculo(char c){
+
+
+        if (Character.isUpperCase(c)){
+            return true;}
+        else{
+            return false;
+            }
+        }
+
+
+    }
+
