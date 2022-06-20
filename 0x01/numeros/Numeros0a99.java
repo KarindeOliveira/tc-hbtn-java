1f2e59138e30e62bbacb24699424498c5eb9e2f2
Karin Pinheiro <karindanoli@gmail.com>
2022-06-20 17:34:21 -0300

    Teste

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -3,7 +3,12 @@ public class Numeros0a99 {
     public static void main(String[] args) {
 
         for(int i=0; i <= 99; i++){
-            System.out.print(i + ", ");
+            if(i == 99){
+            System.out.print(i);}
+            else{
+                System.out.print(i + ", ");
+            }
+
             }
         System.out.println();
 
