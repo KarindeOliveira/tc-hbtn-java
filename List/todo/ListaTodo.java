9a8c86e93917aa610f82fabd3e1cbef9c464a765
Karin Pinheiro <karindanoli@gmail.com>
2022-06-28 16:14:44 -0300

    Teste

---
+++ List/todo/ListaTodo.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class ListaTodo {
+
+    private ArrayList<Tarefa> tarefas;
+    public ListaTodo(){
+        tarefas = new ArrayList<Tarefa>();
+    }
+    public void adicionarTarefa(Tarefa tarefa){
+        boolean existe = false;
+        for(int i = 0; i< tarefas.size(); i++){
+            if(tarefas.get(i).getIdentificador() == tarefa.getIdentificador()){
+                existe = true;
+            }
+        }if(existe == true){
+            throw new IllegalArgumentException("Tarefa com identificador "+tarefa.getIdentificador()+" ja existente na lista");
+        }else{
+            tarefas.add(tarefa);
+        }
+    }
+    public boolean marcarTarefaFeita(int identificador){
+        for(int i = 0; i< tarefas.size(); i++){
+            if(identificador == tarefas.get(i).getIdentificador()){
+                tarefas.get(i).setEstahFeita(true);
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean desfazerTarefa(int identificador){
+        for(int i = 0; i< tarefas.size(); i++){
+            if(identificador == tarefas.get(i).getIdentificador()){
+                tarefas.get(i).setEstahFeita(false);
+                return true;
+            }
+        }
+        return false;
+    }
+    public void desfazerTodas(){
+        for(int i = 0; i< tarefas.size(); i++){
+            tarefas.get(i).setEstahFeita(false);
+        }
+    }
+    public void fazerTodas(){
+        for(int i = 0; i< tarefas.size(); i++){
+            tarefas.get(i).setEstahFeita(true);
+        }
+    }
+    public void listarTarefas(){
+        for(int i = 0; i< tarefas.size(); i++){
+            if(tarefas.get(i).isEstahFeita() == true){
+                System.out.println("[X] Id: "+tarefas.get(i).getIdentificador()+" - Descricao: "+tarefas.get(i).getDescricao());
+            }else{
+                System.out.println("[ ] Id: "+tarefas.get(i).getIdentificador()+" - Descricao: "+tarefas.get(i).getDescricao());
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ List/todo/Tarefa.java
@@ -0,0 +1,57 @@
+public class Tarefa {
+
+    private String descricao;
+
+    private boolean estahFeita;
+
+    private int identificador;
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isEstahFeita() {
+        return estahFeita;
+    }
+
+    public void setEstahFeita(boolean estahFeita) {
+        this.estahFeita = estahFeita;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public Tarefa(String descricao, int identificador){
+
+        modificarDescricao(descricao);
+
+        this.identificador = identificador;
+
+    }
+
+
+    public void modificarDescricao(String descricao){
+
+        if(descricao.equals(null) || descricao.equals("")){
+
+            throw new IllegalArgumentException("Descricao de tarefa invalida");
+
+        }else{
+
+            setDescricao(descricao);
+
+        }
+
+    }
+
+}
