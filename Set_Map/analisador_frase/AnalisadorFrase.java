9f313a666f47349cfd9c9123d2c9a8d02125f94e
Karin Pinheiro <karindanoli@gmail.com>
2022-06-29 13:58:36 -0300

    Teste

---
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeMap;
+
+public class AnalisadorFrase {
+
+
+    public static TreeMap<String,Integer> contagemPalavras(String frase){
+
+        String minuscula = frase.toLowerCase();
+
+        minuscula = minuscula.replaceAll("[!.?]","");
+
+        String[] palavras = minuscula.split(" ");
+
+        TreeMap<String, Integer> repete = new TreeMap<>();
+
+        for (String chave: palavras) {
+            repete.put(chave, Collections.frequency(List.of(palavras),chave));
+        }
+        return repete;
+    }
+}
+
+
