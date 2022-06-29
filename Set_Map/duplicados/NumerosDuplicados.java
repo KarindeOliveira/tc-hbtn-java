95955ba053cf28572cd55c2070de1f55c31d2b9a
Karin Pinheiro <karindanoli@gmail.com>
2022-06-29 15:17:47 -0300

    Teste

---
+++ Set_Map/duplicados/NumerosDuplicados.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+
+
+import java.util.TreeSet;
+
+
+
+public class NumerosDuplicados {
+
+
+
+    public static TreeSet<Integer> buscar(int[] numeros){
+
+        HashSet<Integer> num = new HashSet<Integer>();
+
+        TreeSet<Integer> num2 = new TreeSet<Integer>();
+
+        for (int n :numeros) {
+
+            if(!num.add(n)){
+
+                num2.add(n);
+
+            }
+
+        }
+
+        return num2;
+
+    }
+
+}
+
