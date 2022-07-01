a2a5a7490fb13c103aabe3aa888dc11a1885a50a
Karin Pinheiro <karindanoli@gmail.com>
2022-07-01 09:07:22 -0300

    Teste

--- Collections_Exercicios/postagem_blog/Autor.java
+++ Collections_Exercicios/postagem_blog/Autor.java
@@ -1,55 +1,110 @@
 import java.util.Objects;
 
+
 public class Autor implements Comparable<Autor>{
 
+
+
     private String nome;
+
     private String sobrenome;
 
-    public Autor(String nome, String sobrenome) {
+
+
+    public Autor(String nome, String sobrenome){
+
         this.nome = nome;
+
         this.sobrenome = sobrenome;
+
     }
+
+
+
+    @Override
+
+    public String toString() {
+
+        return this.nome + " " + this.sobrenome;
+
+    }
+
+
 
     public String getNome() {
+
         return nome;
+
     }
+
+
 
     public void setNome(String nome) {
+
         this.nome = nome;
+
     }
+
+
 
     public String getSobrenome() {
+
         return sobrenome;
+
     }
+
+
 
     public void setSobrenome(String sobrenome) {
+
         this.sobrenome = sobrenome;
-    }
 
-    @Override
-    public String toString() {
-        return "Autor{" +
-                "nome='" + nome + '\'' +
-                ", sobrenome='" + sobrenome + '\'' +
-                '}';
     }
 
 
-    @Override
-    public int compareTo(Autor autor) {
-        return this.getNome().compareTo(autor.getNome());
-    }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Autor autor = (Autor) o;
-        return nome.equals(autor.nome) && sobrenome.equals(autor.sobrenome);
+
+    public int compareTo(Autor o) {
+
+        return this.getNome().compareTo(o.getNome());
+
     }
 
+
+
     @Override
+
+    public boolean equals(Object autorObj) {
+
+        if (autorObj == null) return false;
+
+        if (this == autorObj) return true;
+
+        if (this.getClass() != autorObj.getClass()) return false;
+
+
+
+        Autor autor = (Autor) autorObj;
+
+        return this.getNome() != null
+
+                && this.getNome().equals(autor.getNome())
+
+                && this.getSobrenome() != null
+
+                && this.getSobrenome().equals(autor.getSobrenome());
+
+    }
+
+
+
+    @Override
+
     public int hashCode() {
-        return Objects.hash(nome, sobrenome);
+
+        return Objects.hash(nome);
+
     }
+
 }
--- Collections_Exercicios/postagem_blog/Post.java
+++ Collections_Exercicios/postagem_blog/Post.java
@@ -1,52 +1,77 @@
+
 public class Post implements Comparable<Post>{
 
+
     private Autor autor;
+
     private String titulo;
+
     private String corpo;
+
     private Categorias categoria;
 
-    public Post(Autor autor, String titulo, String corpo, Categorias categoria) {
+
+
+    public Post(Autor autor, String titulo, String corpo, Categorias categoria){
+
         this.autor = autor;
+
         this.titulo = titulo;
-        this.corpo = corpo;
+
         this.categoria = categoria;
+
+        this.corpo = corpo;
+
     }
 
-    @Override
-    public String toString() {
-        return "Post{" +
-                "titulo='" + titulo + '\'' +
-                '}';
-    }
 
-    @Override
-    public int compareTo(Post post) {
-        return this.getAutor().compareTo(post.getAutor());
-    }
 
     public Autor getAutor() {
+
         return autor;
-    }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getCorpo() {
-        return corpo;
-    }
-
-    public void setCorpo(String corpo) {
-        this.corpo = corpo;
     }
 
     public Categorias getCategoria() {
+
         return categoria;
+
     }
 
+
+
+    @Override
+
+    public int compareTo(Post post) {
+
+        return this.getTitulo().compareTo(post.getTitulo());
+
+    }
+
+
+
+    public String getTitulo() {
+
+        return titulo;
+
+    }
+
+
+
+    public void setTitulo(String titulo) {
+
+        this.titulo = titulo;
+
+    }
+
+
+
+    @Override
+
+    public String toString(){
+
+        return this.titulo;
+
+    }
 
 }
