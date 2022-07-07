e298abb7c069ab171d95fc536f26e11ce5b79949
Karin Pinheiro <karindanoli@gmail.com>
2022-07-07 08:19:49 -0300

    Teste

---
+++ Generics/Generics_Exercises/armazem/Armazenavel.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public interface Armazenavel<T> {
+
+
+
+        public void adicionarAoInventario(String nome, T valor);
+
+        public T obterDoInventario(String nome);
+
+        }
+
