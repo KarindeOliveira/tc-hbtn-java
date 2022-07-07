8a4b411e1f0eea02293520b5a24684b96b383e9a
Karin Pinheiro <karindanoli@gmail.com>
2022-07-07 08:39:22 -0300

    Teste

---
+++ Generics_Exercises/armazem_especifico/Armazem.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class Armazem<T> implements Armazenavel<T> {
+
+    private Map<String, T> armazenados = new HashMap<>();
+
+    @Override
+    public void adicionarAoInventario(String nome, T valor) {
+        armazenados.put(nome, valor);
+    }
+
+    @Override
+    public T obterDoInventario(String nome) {
+        return armazenados.get(nome);
+    }
+}
