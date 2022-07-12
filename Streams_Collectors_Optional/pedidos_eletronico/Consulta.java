b0d8c685bb495224a75f5805ecf8703b29f7d151
Karin Pinheiro <karindanoli@gmail.com>
2022-07-12 12:28:33 -0300

    Teste

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido){
+
+        return pedido.getProdutos().stream().filter(list -> list.getCategoria() == CategoriaProduto.LIVRO).collect(Collectors.toList());
+
+
+    }
+
+    public static Produto obterProdutoMaiorPreco (List<Produto> produtos){
+
+        return produtos.stream().sorted(comparing(Produto::getPreco).reversed()).limit(1).collect(Collectors.toList()).get(0);
+
+    }
+
+
+    public static List<Produto> obterProdutosPorPrecoMinimo (List<Produto> produtos, int precoMin){
+
+        return produtos.stream().filter(list -> list.getPreco() > precoMin || list.getPreco() == precoMin).collect(Collectors.toList());
+
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedido){
+
+        return pedido.stream().filter(x -> x.getProdutos().stream().anyMatch(pedidos -> pedidos.getCategoria().equals(CategoriaProduto.ELETRONICO))).collect(Collectors.toList());
+
+
+
+    }
+
+
+
+}
