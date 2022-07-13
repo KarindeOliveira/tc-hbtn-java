451143a458a4c46f7e4b05608d4700421af1609f
Karin Pinheiro <karindanoli@gmail.com>
2022-07-13 17:34:37 -0300

    teste

--- Serializacao/serializacao/SerializarEstudantes.java
+++ Serializacao/serializacao/SerializarEstudantes.java
@@ -12,59 +12,42 @@ public class SerializarEstudantes<Estudantes> {
 
     public void serializar(List<Estudante> estudantes) {
         List<Estudante> lista = new ArrayList<>();
-        FileOutputStream arquivo = null;
+
         try {
-            arquivo = new FileOutputStream("c:\\" + this.nomeArquivo + ".ser");
+           FileOutputStream arquivo = new FileOutputStream("c:\\" + this.nomeArquivo + ".ser");
+            ObjectOutputStream oos = new ObjectOutputStream(arquivo);
+            oos.writeObject(estudantes);
+            oos.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Nao foi possivel serializar");
-        }
-        ObjectOutputStream oos = null;
-        try {
-            oos = new ObjectOutputStream(arquivo);
-        } catch (IOException e) {
-            throw new RuntimeException("Nao foi possivel serializar");
-        }
-        try {
-            oos.writeObject(estudantes);
-        } catch (IOException e) {
-            throw new RuntimeException("Nao foi possivel serializar");
-        }
-        try {
-            oos.close();
+
+
         } catch (IOException e) {
             throw new RuntimeException("Nao foi possivel serializar");
         }
 
     }
 
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings({"all"})
     public List<Estudante> desserializar() {
+
         List<Estudante> estudante = new ArrayList<>();
-        FileInputStream fis = null;
         try {
-            fis = new FileInputStream("c:\\" + this.nomeArquivo + ".ser");
+
+        FileInputStream fis = new FileInputStream("c:\\" + this.nomeArquivo + ".ser");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        estudante = (List<Estudante>) ois.readObject();
+        ois.close();
+
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("Nao foi possivel desserializar");
-        }
-        ObjectInputStream ois = null;
-        try {
-            ois = new ObjectInputStream(fis);
+            System.out.println("Nao foi possivel desserializar");
+
         } catch (IOException e) {
-            throw new RuntimeException("Nao foi possivel desserializar");
-        }
-        try {
-            estudante = (List<Estudante>) ois.readObject();
-        } catch (IOException e) {
-            throw new RuntimeException("Nao foi possivel desserializar");
+            System.out.println("Nao foi possivel desserializar");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Nao foi possivel desserializar");
+            throw new RuntimeException(e);
         }
-        try {
-            ois.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Nao foi possivel desserializar");
-        }
-        return estudante;
+       return estudante;
     }
 
 
