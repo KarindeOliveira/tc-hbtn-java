ee44aa86762d16f1a6b0cb171a9b02417eb370ce
Karin Pinheiro <karindanoli@gmail.com>
2022-07-15 08:42:43 -0300

    teste

---
+++ apache-maven/financeiro-api/src/main/java/FinanceiroMain.java
@@ -0,0 +1,9 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class FinanceiroMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Criando um projeto Java Maven.");
+    }
+}
